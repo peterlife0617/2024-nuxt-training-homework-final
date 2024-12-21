ad416f7d2951e214932428ce6851384c8ba486c0
dragon <peterlife0617@gmail.com>
2024-12-21 18:16:12 +0800

    feat(date): 新增中文星期顯示功能

--- utils/date.ts
+++ utils/date.ts
@@ -1,10 +1,29 @@
 import dayjs from 'dayjs'
 
+const weekDayMap = {
+  Monday: '星期一',
+  Tuesday: '星期二',
+  Wednesday: '星期三',
+  Thursday: '星期四',
+  Friday: '星期五',
+  Saturday: '星期六',
+  Sunday: '星期日',
+}
+
 export function formatDate(date: string | Date | undefined | null, template = 'YYYY/MM/DD') {
   if (!date) {
     return ''
   }
-  return dayjs(date).format(template)
+
+  const formattedDate = dayjs(date).format(template)
+
+  if (template.includes('dddd')) {
+    const weekDay = dayjs(date).format('dddd') as keyof typeof weekDayMap
+    const twWeekDay = weekDayMap[weekDay]
+    return formattedDate.replace(weekDay, twWeekDay)
+  }
+
+  return formattedDate
 }
 
 export function diffDays(date1: string | Date | undefined | null, date2: string | Date | undefined | null, absolute: boolean = true) {
